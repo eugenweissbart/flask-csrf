68b6534440b45dad8d320e2efb2f253897c8c6a3
Alex Stapleton <alex@quru.com>
2013-08-12 10:56:52 +0100

    BREACH mitigation
    XORs a nonce into the CSRF field.

--- flaskext/csrf.py
+++ flaskext/csrf.py
@@ -9,8 +9,14 @@
     :license: MIT, see LICENSE for more details.
 """
 
-from uuid import uuid4
+import os
+from base64 import b64encode, b64decode
+
 from flask import abort, request, session, g
+from itsdangerous import (
+    JSONWebSignatureSerializer, constant_time_compare, bytes_to_int,
+    int_to_bytes
+)
 from werkzeug.routing import NotFound
 
 _exempt_views = []
@@ -32,20 +38,46 @@ def csrf(app, on_csrf=None):
     
     @app.before_request
     def _csrf_protect():
-        # This simplifies unit testing, wherein CSRF seems to break
-        if app.config.get('TESTING'):
-            return
-        if not g._csrf_exempt:
-            if request.method == "POST":
-                csrf_token = session.pop('_csrf_token', None)
-                if not csrf_token or csrf_token != request.form.get('_csrf_token'):
-                    if on_csrf:
-                        on_csrf(*app.match_request())
-                    abort(400)
-    
+	if request.method in ("POST", "DELETE", "PUT") and not g._csrf_exempt:
+	    csrf_secret = session.get('_csrf_secret')
+	    csrf_token = request.form.get('_csrf_token')
+
+	    if is_csrf_token_bad(csrf_token, csrf_secret):
+		if on_csrf:
+		    on_csrf(*app.match_request())
+		abort(400)
+   
+    def is_csrf_token_bad(token, csrf_secret):
+	try:
+	    jsw = JSONWebSignatureSerializer(app.secret_key) 
+	    tobj = jsw.loads(token)
+
+	    nonce_int = bytes_to_int(b64decode(tobj["n"]))
+	    key_int = bytes_to_int(b64decode(tobj["k"]))
+
+	    user_secret = int_to_bytes(nonce_int ^ key_int)
+
+	    return not constant_time_compare(
+		user_secret,
+		csrf_secret	
+	    )
+	except Exception as exc:
+	    return True
+
     def generate_csrf_token():
-        if '_csrf_token' not in session:
-            session['_csrf_token'] = str(uuid4())
-        return session['_csrf_token']
-    
+	nonce = os.urandom(16)
+	secret = session.setdefault('_csrf_secret', os.urandom(16))
+
+	nonce_int = bytes_to_int(nonce)
+	secret_int = bytes_to_int(secret)
+
+	jsw = JSONWebSignatureSerializer(app.secret_key) 
+	token = jsw.dumps({
+	    "n": b64encode(nonce),
+	    "k": b64encode(int_to_bytes(nonce_int ^ secret_int))
+	})
+	
+	return token
+
     app.jinja_env.globals['csrf_token'] = generate_csrf_token
+
